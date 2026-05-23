bd7c4daccf4304275bdbf341617d407fb70b561b
Andrew Hutchings <andrew@wolfssl.com>
2026-05-23 15:58:44 +0100

    fix(m68k): avoid DIVS.W overflow UB

--- qemu/target/m68k/op_helper.c
+++ qemu/target/m68k/op_helper.c
@@ -447,10 +447,20 @@ void HELPER(divsw)(CPUM68KState *env, int destr, int32_t den)
     if (den == 0) {
         raise_exception_ra(env, EXCP_DIV0, GETPC());
     }
+    env->cc_c = 0; /* always cleared, even if overflow */
+    if (den == -1 && num == INT32_MIN) {
+        env->cc_v = -1;
+        /* nothing else is modified */
+        /*
+         * real 68040 keeps N and unset Z on overflow,
+         * whereas documentation says "undefined"
+         */
+        env->cc_z = 1;
+        return;
+    }
     quot = num / den;
     rem = num % den;
 
-    env->cc_c = 0; /* always cleared, even if overflow */
     if (quot != (int16_t)quot) {
         env->cc_v = -1;
         /* nothing else is modified */
--- tests/unit/test_m68k.c
+++ tests/unit/test_m68k.c
@@ -87,7 +87,34 @@ static void test_divsl_int32_min_overflow(void)
     OK(uc_close(uc));
 }
 
+static void test_divsw_int32_min_overflow(void)
+{
+    uc_engine *uc;
+    uint8_t code[] = {
+        0x81, 0xfc, 0xff, 0xff,             // divs.w #$ffff,d0
+    };
+    uint32_t d0 = 0x80000000;
+    uint32_t sr = 0x2700;
+
+    uc_common_setup(&uc, UC_ARCH_M68K, UC_MODE_BIG_ENDIAN, code, sizeof(code),
+                    UC_CPU_M68K_M68000);
+
+    OK(uc_reg_write(uc, UC_M68K_REG_D0, &d0));
+    OK(uc_reg_write(uc, UC_M68K_REG_SR, &sr));
+    OK(uc_emu_start(uc, code_start, code_start + sizeof(code), 0, 0));
+
+    OK(uc_reg_read(uc, UC_M68K_REG_D0, &d0));
+    OK(uc_reg_read(uc, UC_M68K_REG_SR, &sr));
+
+    TEST_CHECK(d0 == 0x80000000);
+    TEST_CHECK((sr & 0x0002) == 0x0002);
+    TEST_CHECK((sr & 0x0001) == 0);
+
+    OK(uc_close(uc));
+}
+
 TEST_LIST = {{"test_move_to_sr", test_move_to_sr},
              {"test_sr_contains_flags", test_sr_contains_flags},
              {"test_divsl_int32_min_overflow", test_divsl_int32_min_overflow},
+             {"test_divsw_int32_min_overflow", test_divsw_int32_min_overflow},
              {NULL, NULL}};
